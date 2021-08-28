2c4d381746e19bc263dd2dade85e28e9942b21df
James Pan <pan.mamkkl@gmail.com>
2021-08-28 16:03:28 +0800

    [no-jira] chore: fix bugs

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. Review i18n
 2. Define startup procedure
+3. [Bug] Custom fields can't display the field title in "Address" section of the Admin portal
--- upload/admin/language/en-gb/common/header.php
+++ upload/admin/language/en-gb/common/header.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']      = 'OpenCart';
+$_['heading_title']      = 'Wittisys eCommerce';
 
 // Text
 $_['text_profile']       = 'Your Profile';
